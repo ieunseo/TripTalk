edeaefca95603e9dfbe418a1761c427256fd79e2
ieunseo <y949512@naver.com>
2026-08-26 22:08:46 +0900

    #3 - 마이페이지 거래내역, 북마크 추가

--- trip_homework/src/app/(main)/mypage/page.tsx
+++ trip_homework/src/app/(main)/mypage/page.tsx
@@ -1,28 +1,69 @@
-import styles from "./styles.module.css";
-import HeroBanner from "@/components/home/hero-banner";
+"use client";
 
-export default function MyPage(){
-    return (
-        <div>
-            <h1>마이페이지</h1>
-            <section>
-                <p>내정보</p>
-                <div>
-                    <img src="/images/a.png" className="profileImg" />
-                    <p>이은서</p>
-                </div>
-                <hr />
-                <div className={styles.point}>
-                    23,000P
-                </div>
-            </section>
-            <button>거래내역&북마크</button>
-            <button>포인트 사용 내역</button>
-            <button>비밀번호 변경</button>
-            {/*랜더링영역*/}
+import { useState } from "react";
+
+import MyPoint from "@/components/mypage/my-point";
+import MyProduct from "@/components/mypage/my-product";
+import styles from "./styles.module.css";
+
+export default function MyPage() {
+  const [menu, setMenu] = useState("point");
+  const user = {
+      name: "은서띠띠",
+      point:123123,
+      image:"",
+  };
+
+  return (
+    <main className={styles.page}>
+      <h1>마이 페이지</h1>
+
+      <section className={styles.info}>
+        <strong>내 정보</strong>
+
+        <div className={styles.user}>
+          <div className={styles.image}>{user.image}</div>
+          <span>{user.name}</span>
         </div>
 
+        <div className={styles.line} />
 
-    )
+          <div className={styles.pointSection}>  <img src="/icons/point.svg"/><p className={styles.point}>{user.point}</p></div>
 
+        <div className={styles.line} />
+
+          <button
+              className={menu === "product" ? styles.selected : ""}
+              onClick={() => setMenu("product")}
+          >
+              거래내역 & 북마크　&gt;
+          </button>
+
+          <button
+              className={menu === "point" ? styles.selected : ""}
+              onClick={() => setMenu("point")}
+          >
+              포인트 사용 내역　&gt;
+          </button>
+        <button>비밀번호 변경　&gt;</button>
+      </section>
+
+      <div className={styles.menu}>
+        <button
+          className={menu === "point" ? styles.active : ""}
+          onClick={() => setMenu("point")}
+        >
+          포인트 사용 내역
+        </button>
+        <button
+          className={menu === "product" ? styles.active : ""}
+          onClick={() => setMenu("product")}
+        >
+          나의 상품 & 북마크
+        </button>
+      </div>
+
+      {menu === "point" ? <MyPoint /> : <MyProduct />}
+    </main>
+  );
 }
