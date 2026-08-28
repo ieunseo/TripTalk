327c5c668eb3f3d334317d3f3673fab9f5826cfe
ieunseo <y949512@naver.com>
2026-08-28 17:12:20 +0900

    #5 숙박 상세화 - layout

--- trip_homework/src/app/(main)/travelproducts/details/page.tsx
+++ trip_homework/src/app/(main)/travelproducts/details/page.tsx
@@ -12,19 +12,27 @@ export default function TravelProductsDetailPage() {
         <span>한줄설명</span>
         <p>태그</p>
         <div>
-            <Image src={} alt={}/>
+            <Image src={"/images/a.png"} alt={""}/>
             <div>
                 <div>0000원</div>
                 <div>- 숙박권은 트립트립에서 포인트 충전후 구매할수있습니다</div>]
                 <div> - 상세설명에 숙박권 사용기한을 꼭 확인해주세요.</div>
-            </div>
-            <div className={styles.detail}>
-                <div>상세설명</div>
-                <div>ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ</div>
+                <button>구매하기</button>
             </div>
             <div>
-                <div>상세위치</div>
+                판매자
             </div>
+        </div>
+        <div className={styles.detail}>
+            <div>상세설명</div>
+            <div>ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ</div>
+        </div>
+        <div>
+            <div>상세위ㅑ</div>
+        </div>
+        <div>
+            문의하기
+            <textarea/>
         </div>
 
     </main>);
